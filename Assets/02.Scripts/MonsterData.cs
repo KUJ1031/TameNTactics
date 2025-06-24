2df3b331637736625aa9adaba9d52c5f7f291741
Won0001 <smjwnaya1102@gmail.com>
2025-06-24 11:55:27 +0900

    feat : 스킬데이터 생성
    - 스크립터블오브젝트로 스킬데이터 생성

--- Assets/02.Scripts/MonsterData.cs
+++ Assets/02.Scripts/MonsterData.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewMonster", menuName = "Monster/MonsterData")]
 public class MonsterData : ScriptableObject
 {
+    [Header("몬스터 기본 정보")]
     public string name;
     public int maxHP;
     public int attackPower;
     public int defense;
     public int speed;
     public int criticalChance;
-    public Sprite monsterSprite;
     public ElementType elementType;
     
+    [Header("스킬 설정")]
     public SkillData passiveSkill;
-    public SkillData[] activeSkills;
+    public SkillData activeSkill;
+    public SkillData ultimateSkill;
 }
 
 public enum ElementType
--- Assets/02.Scripts/SkillData.cs
+++ Assets/02.Scripts/SkillData.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillData : MonoBehaviour
+[CreateAssetMenu(fileName = "New Skill", menuName = "Skill/Skill Data")]
+public class SkillData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("스킬 기본 정보")]
+    public string name;
+    // 파워가 1.5라면 150%로 작성해주세요
+    public string description;
+    // 파워는 1.5 이런식으로 적용해주세요
+    public float power;
+    public SkillType skillType;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("궁극기 코스트")]
+    public int ultimateCost;
+}
+
+public enum SkillType
+{
+    Passive,
+    Active,
+    Ultimate
 }
