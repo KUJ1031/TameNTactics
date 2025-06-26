1c71a55ce9b18cbee899cfad095fb948dfa8e06d
KGH1125 <128346998+KGH1125@users.noreply.github.com>
2025-06-26 15:13:20 +0900

    docs : 아이템데이터 수정
    가격과 설명 추가, 리소스 -> 제스쳐타입으로 변경

--- Assets/07.ScriptableObjects/Data/03.ItemData/ItemData.cs
+++ Assets/07.ScriptableObjects/Data/03.ItemData/ItemData.cs
@@ -19,7 +19,7 @@ public enum ItemType
 {
     equipment,  //장착
     consumable, //소모
-    resource    //기타
+    gesture     //제스쳐
 }
 
 [CreateAssetMenu(fileName = "NewItem", menuName = "New Item/Create New Item")]
@@ -29,7 +29,10 @@ public class ItemData : ScriptableObject
     [Header("기본 정보")]
     public string itemName;     //아이템 이름
     public Sprite itemImage;    //아이템 이미지
+    public int maxStack;        //최대 중복 스택
     public ItemType type;       //장착,소모,기타
+    public string description;  //아이템 설명
+    public int goldValue;       //가격
 
     [Header("효과")]
     public List<ItemEffect> itemEffects;//아이템 효과
