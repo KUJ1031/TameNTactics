9fdaa7133a441f37d6fca3787b2febf556a4e1fd
Won0001 <smjwnaya1102@gmail.com>
2025-06-26 19:19:08 +0900

    feat : 베이스배틀스테이트 생성, 배틀시스템 생성

--- Assets/02.Scripts/Battle/BattleSystem.cs
+++ Assets/02.Scripts/Battle/BattleSystem.cs
@@ -1,7 +1,24 @@
-using System;
 using UnityEngine;
 
 public class BattleSystem : MonoBehaviour
 {
-    
+    private BaseBattleState currentState;
+    public BattleManager BattleManager;
+
+    private void Start()
+    {
+        BattleManager = BattleManager.Instance;
+    }
+
+    private void Update()
+    {
+        currentState?.Execute();
+    }
+
+    public void ChangeState(BaseBattleState newState)
+    {
+        currentState?.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
 }
