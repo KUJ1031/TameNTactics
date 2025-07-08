89175a31cb3b16698d57f52c2da4d5bf37409913
JangHoon Ban <janghoon333@naver.com>
2025-07-08 12:23:28 +0900

    feat : 도망가기 기능 추가, 트리거 일정시간 삭제 기능 수정중

--- Assets/02.Scripts/Battle/PlayerBattleTrigger.cs
+++ Assets/02.Scripts/Battle/PlayerBattleTrigger.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 public class PlayerBattleTrigger : MonoBehaviour
 {
@@ -8,10 +9,10 @@ public class PlayerBattleTrigger : MonoBehaviour
         //충돌 객체 정보 가지고오기
         MonsterFactory factory = other.GetComponentInParent<MonsterFactory>();
         if (factory == null) return;
-        
+
         MonsterCharacter character = other.GetComponent<MonsterCharacter>();
         if (character == null) return;
-        
+
         Monster monster = character.monster;
         if (monster == null) return;
 
@@ -23,10 +24,26 @@ public class PlayerBattleTrigger : MonoBehaviour
         BattleManager.Instance.enemyTeam = enemyTeam;
 
         Destroy(other.gameObject); // 충돌한 적 제거
-        
+
         RuntimePlayerSaveManager.Instance.SaveCurrentGameState(PlayerManager.Instance.player); // 현재 플레이어 상태 저장
 
         //씬이동
         UnityEngine.SceneManagement.SceneManager.LoadScene("BattleUITest");
+    }
+
+    public void DisableTriggerTemporarily(float disableTime)
+    {
+        Debug.Log("트리거 없애기 진입");
+        StartCoroutine(DisableTriggerCoroutine(disableTime));
+    }
+
+    private IEnumerator DisableTriggerCoroutine(float time)
+    {
+        Collider2D collider = GetComponent<Collider2D>();
+        collider.enabled = false;
+
+        yield return new WaitForSeconds(time);
+
+        collider.enabled = true;
     }
 }
