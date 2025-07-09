b0e27856298d97eb70efecf943c5276b0b05227b
JangHoon Ban <janghoon333@naver.com>
2025-07-09 12:59:39 +0900

    fix : 스킬 선택시 패시브 스킬이 선택되는 문제 수정

--- Assets/02.Scripts/UI/View/SkillView.cs
+++ Assets/02.Scripts/UI/View/SkillView.cs
@@ -10,34 +10,14 @@ public class SkillView : MonoBehaviour
     [SerializeField] private Image skillSlot2;
     [SerializeField] private Transform skillPanel;
 
-    // 테스트용 코드
-    [SerializeField] private TextMeshProUGUI skillName1;
-    [SerializeField] private TextMeshProUGUI skillName2;
-
     public void ShowSkillList(List<SkillData> skills)
     {
-        Debug.Log("몬스터가 가지고 있는 스킬을 보여줍니다.");
-        for (int i = 0; i < skills.Count; i++)
-        {
-            Debug.Log($"{i + 1}번째 스킬 타입 : {skills[i].skillType}");
-        }
-
         // 1번 슬롯
-        if (skills.Count >= 1 && skills[1] != null)
+        if (skills.Count >= 2 && skills[1] != null)
         {
+            var selectSlot = skillSlot1.GetComponent<SkillSelecter>();
             skillSlot1.sprite = skills[1].icon;
-            if (skills[1].skillType == SkillType.NormalSkill)
-            {
-                skillName1.text = "N";
-            }
-            else if (skills[1].skillType == SkillType.UltimateSkill)
-            {
-                skillName1.text = "U";
-            }
-            else
-            {
-                skillName1.text = "P";
-            }
+            selectSlot.skillIndex = 1;
         }
         else if (skills[1] == null)
         {
@@ -45,21 +25,11 @@ public class SkillView : MonoBehaviour
         }
 
         // 2번 슬롯
-        if (skills.Count >= 2 && skills[2] != null)
+        if (skills.Count >= 3 && skills[2] != null)
         {
+            var selectSlot = skillSlot2.GetComponent<SkillSelecter>();
             skillSlot2.sprite = skills[2].icon;
-            if (skills[2].skillType == SkillType.NormalSkill)
-            {
-                skillName1.text = "N";
-            }
-            else if (skills[2].skillType == SkillType.UltimateSkill)
-            {
-                skillName1.text = "U";
-            }
-            else
-            {
-                skillName1.text = "P";
-            }
+            selectSlot.skillIndex = 2;
         }
         else if (skills[2] == null)
         {
