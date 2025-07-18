50ca4107d5ac2743000560305d59d4d1497a07a8
KUJ <thsusekdnlt1@naver.com>
2025-07-18 10:03:05 +0900

    feat : 초기 맵 추가 및 캐릭터 에셋 추가
    초기 맵 추가 및 캐릭터 에셋 추가

---
+++ Assets/SPUM/Sample/Script/Editor/PlayerManagerEditor.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace AssetPack
+{
+    [CustomEditor(typeof(PlayerManager))]
+    public class PlayerManagerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            PlayerManager manager = (PlayerManager)target;
+
+            if (GUILayout.Button("CREATE UNIT"))
+            {
+                manager.GetPlayerList();
+            }
+            if (GUILayout.Button("Align UNIT"))
+            {
+                manager.SetAlignUnits();
+            }
+            if (GUILayout.Button("CLEAR UNIT"))
+            {
+                manager.ClearPlayerList();
+            }
+            if (GUILayout.Button("CAPTURE UNITS"))
+            {
+                manager.SetScreenShot();
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
