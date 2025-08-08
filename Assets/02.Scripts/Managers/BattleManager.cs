d65c205404cf5582255e05aa59b5ae540d508188
KGH1125 <128346998+KGH1125@users.noreply.github.com>
2025-08-08 21:14:12 +0900

    designe : StartSecne 수정

--- Assets/02.Scripts/Managers/BattleManager.cs
+++ Assets/02.Scripts/Managers/BattleManager.cs
@@ -354,9 +354,9 @@ public class BattleManager : Singleton<BattleManager>
             Vector2 attackPos = AttackPosition.transform.position;
 
             casterChar.PlayAttack();
-
             yield return StartCoroutine(MoveToPosition(casterChar, attackPos, 0.3f, false));
             casterChar.PlayIdle();
+
 
             ISkillEffect effect = null;
 
